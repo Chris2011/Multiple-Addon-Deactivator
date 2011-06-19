95446a1109401ff01db97c27c587660c8a6c611e
Chris2011 <christian.lenz@gmx.net>
2011-06-19 18:58:29 +0200

    mad extensions

--- build/testMozilla Firefox/extensions/MAD@ChrisLE.de/content/scripts/overlay.js
+++ build/testMozilla Firefox/extensions/MAD@ChrisLE.de/content/scripts/overlay.js
@@ -1,17 +1,48 @@
-var MAD  = {
-  onLoad: function() {
-    // initialization code
-    this.initialized = true;
-    this.gfiltersimportexportBundle = Components.classes["@mozilla.org/intl/stringbundle;1"].getService(Components.interfaces.nsIStringBundleService);
-    this.mystrings = this.gfiltersimportexportBundle.createBundle("chrome://MAD/locale/overlay.properties");
-},
-  onToolbarButtonCommand: function() {
+var MAD = {
+   onLoad: function()
+   {
+      // initialization code
+      this.initialized = true;
+      this.gfiltersimportexportBundle = Components.classes["@mozilla.org/intl/stringbundle;1"].getService(Components.interfaces.nsIStringBundleService);
+      this.mystrings = this.gfiltersimportexportBundle.createBundle("chrome://MAD/locale/overlay.properties");
+
+      var myButtonId = "my-mad-extension-button"; // ID of button to add
+      var afterId = "urlbar-container";    // ID of element to insert after
+      var navBar  = document.getElementById("nav-bar");
+      var curSet  = navBar.currentSet.split(",");
+
+      if(curSet.indexOf(myButtonId) == -1)
+      {
+         var pos = curSet.indexOf(afterId) + 1 || curSet.length;
+         var set = curSet.slice(0, pos).concat(myButtonId).concat(curSet.slice(pos));
+
+         navBar.setAttribute("currentset", set.join(","));
+         navBar.currentSet = set.join(",");
+         document.persist(navBar.id, "currentset");
+
+         try
+         {
+            BrowserToolboxCustomizeDone(true);
+         }
+         catch (e)
+         {
+            Application.console.log(e);
+         }
+      }
+   },
+
+   onToolbarButtonCommand: function()
+   {
       window.open("chrome://MAD/content/window.xul", "", "chrome,titlebar,toolbar,centerscreen,modal");
-  },
-  
-  onMenuItemCommand: function(event) {
+   },
+
+   onMenuItemCommand: function(event)
+   {
       window.open("chrome://MAD/content/window.xul", "", "chrome,titlebar,toolbar,centerscreen,modal");
-  }
+   }
 };
 
-window.addEventListener("load", function(e) { MAD.onLoad(e); }, false);
+window.addEventListener("load", function(e)
+{
+   MAD.onLoad(e);
+}, false);
--- src/content/scripts/overlay.js
+++ src/content/scripts/overlay.js
@@ -1,17 +1,48 @@
-var {appname}  = {
-  onLoad: function() {
-    // initialization code
-    this.initialized = true;
-    this.gfiltersimportexportBundle = Components.classes["@mozilla.org/intl/stringbundle;1"].getService(Components.interfaces.nsIStringBundleService);
-    this.mystrings = this.gfiltersimportexportBundle.createBundle("chrome://{appname}/locale/overlay.properties");
-},
-  onToolbarButtonCommand: function() {
+var {appname} = {
+   onLoad: function()
+   {
+      // initialization code
+      this.initialized = true;
+      this.gfiltersimportexportBundle = Components.classes["@mozilla.org/intl/stringbundle;1"].getService(Components.interfaces.nsIStringBundleService);
+      this.mystrings = this.gfiltersimportexportBundle.createBundle("chrome://{appname}/locale/overlay.properties");
+
+      var myButtonId = "my-mad-extension-button"; // ID of button to add
+      var afterId = "urlbar-container";    // ID of element to insert after
+      var navBar  = document.getElementById("nav-bar");
+      var curSet  = navBar.currentSet.split(",");
+
+      if(curSet.indexOf(myButtonId) == -1)
+      {
+         var pos = curSet.indexOf(afterId) + 1 || curSet.length;
+         var set = curSet.slice(0, pos).concat(myButtonId).concat(curSet.slice(pos));
+
+         navBar.setAttribute("currentset", set.join(","));
+         navBar.currentSet = set.join(",");
+         document.persist(navBar.id, "currentset");
+
+         try
+         {
+            BrowserToolboxCustomizeDone(true);
+         }
+         catch (e)
+         {
+            Application.console.log(e);
+         }
+      }
+   },
+
+   onToolbarButtonCommand: function()
+   {
       window.open("chrome://{appname}/content/window.xul", "", "chrome,titlebar,toolbar,centerscreen,modal");
-  },
-  
-  onMenuItemCommand: function(event) {
+   },
+
+   onMenuItemCommand: function(event)
+   {
       window.open("chrome://{appname}/content/window.xul", "", "chrome,titlebar,toolbar,centerscreen,modal");
-  }
+   }
 };
 
-window.addEventListener("load", function(e) { {appname}.onLoad(e); }, false);
+window.addEventListener("load", function(e)
+{
+   {appname}.onLoad(e);
+}, false);
