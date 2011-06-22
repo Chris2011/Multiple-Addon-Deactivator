ce05e4375c4119683b80556d21d14f67984ce950
Chris2011 <christian.lenz@gmx.net>
2011-06-22 19:46:20 +0200

    Added support for Seamonkey 2.1.

---
+++ build/testMozilla Firefox/extensions/ChrisLE@mozilla.org/defaults/preferences/foxbeansdef.js
@@ -0,0 +1,3 @@
+pref("multiple-addon-deactivator.ChrisLE@mozilla.org.description", "chrome://MAD/locale/description.properties");
+pref("multiple-addon-deactivator.ChrisLE@mozilla.org.excludeMAD", false);
+
