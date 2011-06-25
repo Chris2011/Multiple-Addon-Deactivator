04e84939a8598ca8da18f295b36dd9930028a172
Chris2011 <christian.lenz@gmx.net>
2011-06-25 15:48:04 +0200

    newer version 1.1.1 bugfixes

--- build/chrome/defaults/preferences/foxbeansdef.js
+++
@@ -1,3 +0,0 @@
-pref("extensions.multiple-addon-deactivator.ChrisLE@mozilla.org.description", "chrome://MAD/locale/description.properties");
-pref("extensions.multiple-addon-deactivator.ChrisLE@mozilla.org.excludeMAD", false);
-
