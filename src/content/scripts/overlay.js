ac11176dbec53fec283d152286c33befebc4afba
Chris2011 <christian.lenz@gmx.net>
2011-06-26 20:43:00 +0200

    1.2.2 Remove the defaultbutton.

--- src/content/scripts/overlay.js
+++ src/content/scripts/overlay.js
@@ -1,36 +1,14 @@
 var {appname} = {
+   Cc: Components.classes,
+   Ci: Components.interfaces,
    onLoad: function()
    {
       // initialization code
       this.initialized = true;
-      this.gfiltersimportexportBundle = Components.classes["@mozilla.org/intl/stringbundle;1"]
-                                                  .getService(Components.interfaces.nsIStringBundleService);
+      this.gfiltersimportexportBundle = Cc["@mozilla.org/intl/stringbundle;1"]
+                                          .getService(Ci.nsIStringBundleService);
       this.mystrings = this.gfiltersimportexportBundle
                            .createBundle("chrome://{appname}/locale/overlay.properties");
-
-      var myButtonId = "mad-toolbar-button"; // ID of button to add
-      var afterId = "urlbar-container";    // ID of element to insert after
-      var navBar  = document.getElementById("nav-bar");
-      var curSet  = navBar.currentSet.split(",");
-
-      if(curSet.indexOf(myButtonId) == -1)
-      {
-         var pos = curSet.indexOf(afterId) - 1 || curSet.length;
-         var set = curSet.slice(0, pos).concat(myButtonId).concat(curSet.slice(pos));
-
-         navBar.setAttribute("currentset", set.join(","));
-         navBar.currentSet = set.join(",");
-         document.persist(navBar.id, "currentset");
-
-         try
-         {
-            BrowserToolboxCustomizeDone(true);
-         }
-         catch(e)
-         {
-            Application.console.log(e);
-         }
-      }
    },
 
    onToolbarButtonCommand: function(event)
